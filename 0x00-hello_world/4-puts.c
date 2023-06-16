b422eea29b78a8e417cf64ec39c86a2723a910f0
Abdul Sultan <ideasimperial@gmail.com>
2023-06-15 21:21:55 -0700

    added a c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* 'main': is the entry point  
+ *
+ * 'return': will always be 0 (success)
+ */
+
+int main(void)
+{
+     puts("\"Programming is like building a multilingual puzzle")
+     return (0);
+}     
