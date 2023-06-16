30e2c8748d9b34d93476e8cdc54c9aad02ca2d99
Abdul Sultan <ideasimperial@gmail.com>
2023-06-15 21:45:23 -0700

    updated the printf.c program file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(void)
 /*
- * Main - is the entry point
- * Return - alway outputs 0 (pass)
+ * main: is the entry point
+ * Return: alway outputs 0 (pass)
  */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
