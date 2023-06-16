959094de2e34f73b42a57e28b08506d975465b3c
Abdul Sultan <ideasimperial@gmail.com>
2023-06-15 21:23:28 -0700

    updated the c program comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-
 /* 'main': is the entry point  
  *
  * 'return': will always be 0 (success)
  */
-
-int main(void)
 {
      puts("\"Programming is like building a multilingual puzzle")
      return (0);
