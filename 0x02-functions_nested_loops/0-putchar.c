9e547ebea4086c9474a01a4626a7eecd54682615
Abdul Sultan <ideasimperial@gmail.com>
2023-06-21 12:45:05 -0700

    added a program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar followed by a new line.
+ *
+ * Return - Always returns 0 (Success)
+ *
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return 0;
+}
