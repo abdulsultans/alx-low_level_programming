f3965339a1020e03bbc6992b836d68bbd7a56c65
Abdul Sultan <askmesultan@gmail.com>
2025-04-04 00:24:45 +0000

    added a funciton that prints a square

---
+++ 0x04-more_functions_mested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square using the character #
+ * @size: Size of the square
+ */
+void print_square(int size)
+{
+    int i, j;
+
+    if (size <= 0)
+    {
+        _putchar('\n');
+    }
+    else
+    {
+        for (i = 0; i < size; i++)
+        {
+            for (j = 0; j < size; j++)
+            {
+                _putchar('#');
+            }
+            _putchar('\n');
+        }
+    }
+}
+
