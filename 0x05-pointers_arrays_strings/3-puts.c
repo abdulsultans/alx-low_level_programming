b2666d8d946c0c125a4aad3a9b39918356cdff48
Abdul Sultan <ideasimperial@gmail.com>
2025-04-06 22:03:12 +0000

    added a function that prints a string followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void _puts(char *str)
+{
+    // Loop through each character in the string and print it
+    while (*str != '\0') {
+        putchar(*str);
+        str++;  // Move to the next character in the string
+    }
+    
+    // Print a newline after the string
+    putchar('\n');
+}
+
+int main()
+{
+    char str[] = "Hello, World!";
+    
+    // Call _puts to print the string followed by a newline
+    _puts(str);
+    
+    return 0;
+}
+
