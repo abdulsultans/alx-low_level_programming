886dbd738230c46bd5061757de5ab602420e4d8d
Abdul Sultan <ideasimperial@gmail.com>
2025-04-06 21:53:18 +0000

    added a function that returns the lenght of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int _strlen(char *s)
+{
+    int length = 0;
+
+    // Loop through the string until we encounter the null terminator '\0'
+    while (s[length] != '\0') {
+        length++;
+    }
+
+    return length;
+}
+
+int main()
+{
+    char str[] = "Hello, World!";
+    
+    // Call _strlen to get the length of the string
+    int len = _strlen(str);
+    
+    printf("The length of the string is: %d\n", len);
+    
+    return 0;
+}
+
