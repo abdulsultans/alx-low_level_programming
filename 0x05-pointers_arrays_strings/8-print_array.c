728f7741448c7aa661125655f7b36728eb87d5cd
Abdul Sultan <ideasimperial@gmail.com>
2025-04-08 07:14:19 +0000

    added a function that prints an array followed by a new line

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void print_array(int *a, int n)
+{
+    for (int i = 0; i < n; i++) {
+        printf("%d", a[i]);
+        if (i < n - 1) {
+            printf(", ");
+        }
+    }
+    printf("\n");
+}
+
