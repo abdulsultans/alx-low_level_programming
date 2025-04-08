dcfe4c42975dc8c5e9c7295139133d66eb288346
Abdul Sultan <ideasimperial@gmail.com>
2025-04-08 07:25:27 +0000

    added a function that copies a string from src to dest, including the null terminator (\0), and returns the pointer to dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+char *_strcpy(char *dest, char *src)
+{
+    int i = 0;
+
+    // Copy characters from src to dest, including the null byte
+    while (src[i] != '\0') {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';  // Add the null terminator at the end
+
+    return dest;  // Return the pointer to dest
+}
+
