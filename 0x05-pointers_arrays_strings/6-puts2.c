7db1fd0d62293a430a715b40fec7fa60e3801b30
Abdul Sultan <ideasimperial@gmail.com>
2025-04-11 21:30:24 +0000

    added a function that prints every other string followed by a new line

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,7 +4,8 @@ void puts2(char *str)
 {
     // Loop through the string, but increment the index by 2 each time
     int i = 0;
-    while (str[i] != '\0') {
+    while (str[i] != '\0') 
+    {
         // Print every other character starting with the first one
         putchar(str[i]);
         i += 2;  // Skip the next character
